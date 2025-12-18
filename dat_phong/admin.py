8c301eb996ca42acb2f76e05d4d989bbe0accc6c
huyhandsome6996 <iamdoinb6996@gmail.com>
2025-12-19 00:12:15 +0700

    Chinh sua model dat phong don gian

--- dat_phong/admin.py
+++ dat_phong/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import Khach, DatPhong
-
-
-@admin.register(Khach)
-class KhachAdmin(admin.ModelAdmin):
-    list_display = ('ten_khach', 'loai_khach')
+from .models import DatPhong
 
 
 @admin.register(DatPhong)
 class DatPhongAdmin(admin.ModelAdmin):
-    list_display = ('phong', 'khach', 'ngay_nhan', 'dang_o')
+    list_display = (
+        'phong',
+        'ten_khach',
+        'loai_khach',
+        'ngay_nhan',
+        'ngay_tra',
+        'dang_o',
+    )
+
+    list_filter = ('loai_khach', 'dang_o')
+    search_fields = ('ten_khach',)
+
--- dat_phong/models.py
+++ dat_phong/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from khach_san.models import Phong
 
 
-class Khach(models.Model):
+from django.db import models
+from khach_san.models import Phong
+
+
+class DatPhong(models.Model):
     LOAI_KHACH_CHOICES = [
         ('vang_lai', 'Vãng lai'),
         ('tour', 'Khách tour đoàn'),
@@ -11,19 +15,38 @@ class Khach(models.Model):
         ('traveloka', 'Traveloka'),
     ]
 
-    ten_khach = models.CharField(max_length=100)
-    loai_khach = models.CharField(max_length=20, choices=LOAI_KHACH_CHOICES)
+    phong = models.ForeignKey(
+        Phong,
+        on_delete=models.PROTECT,
+        verbose_name="Phòng"
+    )
+
+    ten_khach = models.CharField(
+        max_length=100,
+        verbose_name="Tên khách"
+    )
+
+    loai_khach = models.CharField(
+        max_length=20,
+        choices=LOAI_KHACH_CHOICES,
+        verbose_name="Loại khách"
+    )
+
+    ngay_nhan = models.DateField(
+        verbose_name="Ngày nhận phòng"
+    )
+
+    ngay_tra = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name="Ngày trả phòng"
+    )
+
+    dang_o = models.BooleanField(
+        default=True,
+        verbose_name="Đang ở"
+    )
 
     def __str__(self):
-        return self.ten_khach
+        return f"{self.phong.ma_phong} - {self.ten_khach}"
 
-
-class DatPhong(models.Model):
-    phong = models.ForeignKey(Phong, on_delete=models.PROTECT)
-    khach = models.ForeignKey(Khach, on_delete=models.PROTECT)
-    ngay_nhan = models.DateField()
-    ngay_tra = models.DateField(null=True, blank=True)
-    dang_o = models.BooleanField(default=True)
-
-    def __str__(self):
-        return f"{self.phong} - {self.khach}"
