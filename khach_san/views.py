ae564a2e92489e61af27ffcb10a7e8e3b0e2cf54
huyhandsome6996 <iamdoinb6996@gmail.com>
2025-12-18 23:48:08 +0700

    Tinh tien khi check-out phong

--- khach_san/views.py
+++ khach_san/views.py
@@ -28,8 +28,21 @@ def check_in(request, ma_phong):
 def check_out(request, ma_phong):
     phong = get_object_or_404(Phong, ma_phong=ma_phong)
 
-    if phong.trang_thai == 'dang_thue':
+    if request.method == 'POST':
+        so_dem = int(request.POST.get('so_dem', 1))
+        gia = phong.loai_phong.gia_mot_dem
+        tong_tien = so_dem * gia
+
         phong.trang_thai = 'trong'
         phong.save()
 
-    return redirect('khach_san:chi_tiet_phong', ma_phong=ma_phong)
+        return render(request, 'khach_san/ket_qua_thanh_toan.html', {
+            'phong': phong,
+            'so_dem': so_dem,
+            'gia': gia,
+            'tong_tien': tong_tien
+        })
+
+    return render(request, 'khach_san/xac_nhan_check_out.html', {
+        'phong': phong
+    })
