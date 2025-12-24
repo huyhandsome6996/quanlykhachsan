f5fa25b5b02f8f78b415f518c2e4a63ef5972308
NyenT <vbtnguyen2k6@gmail.com>
2025-12-24 17:09:01 +0700

    thiet ke ham checkout 1

--- dat_phong/views.py
+++ dat_phong/views.py
@@ -49,71 +49,8 @@ def tao_dat_phong(request):
 # =========================
 
 def check_out(request, dat_phong_id):
-    dat_phong = get_object_or_404(DatPhong, id=dat_phong_id, dang_o=True)
-    
-
-    ngay_tra = timezone.now().date()
-    so_dem = (ngay_tra - dat_phong.ngay_nhan).days
-    if so_dem <= 0:
-        so_dem = 1
-
-    gia_mot_dem = dat_phong.phong.loai_phong.gia_mot_dem
-    ds_dich_vu = SuDungDichVu.objects.filter(dat_phong=dat_phong)
-    tong_dich_vu = sum(dv.thanh_tien() for dv in ds_dich_vu)
-
-    tong_tien = so_dem * gia_mot_dem + tong_dich_vu
-
-    if request.method == 'POST':
-        dat_phong.ngay_tra = ngay_tra
-        dat_phong.dang_o = False
-        dat_phong.save()
-
-        phong = dat_phong.phong
-        phong.trang_thai = 'trong'
-        phong.save()
-
-        return redirect('bao_cao:trang_chu')
-
-    context = {
-        'dat_phong': dat_phong,
-        'so_dem': so_dem,
-        'gia_mot_dem': gia_mot_dem,
-        'ds_dich_vu': ds_dich_vu,
-        'tong_dich_vu': tong_dich_vu,
-        'tong_tien': tong_tien
-    }
-    return render(request, 'dat_phong/checkout.html', context)
-
-
-def them_dich_vu(request, dat_phong_id):
-    dat_phong = get_object_or_404(DatPhong, id=dat_phong_id, dang_o=True)
-    danh_sach_dich_vu = DichVu.objects.all()
-
-    if request.method == 'POST':
-        dich_vu_id = request.POST.get('dich_vu')
-        so_luong = int(request.POST.get('so_luong', 1))
-
-        dich_vu = get_object_or_404(DichVu, id=dich_vu_id)
-
-        SuDungDichVu.objects.create(
-            dat_phong=dat_phong,
-            dich_vu=dich_vu,
-            so_luong=so_luong
-        )
-
-        return redirect('khach_san:chi_tiet_phong', ma_phong=dat_phong.phong.ma_phong)
-
-    context = {
-        'dat_phong': dat_phong,
-        'danh_sach_dich_vu': danh_sach_dich_vu
-    }
-    return render(request, 'dat_phong/them_dich_vu.html', context)
-
-def danh_sach_dat_phong(request):
-    danh_sach = DatPhong.objects.select_related('phong') \
-        .order_by('-dang_o', '-ngay_nhan')
-
-    context = {
-        'danh_sach': danh_sach
-    }
-    return render(request, 'dat_phong/danh_sach_dat_phong.html', context)
+    dat_phong = get_object_or_404(
+        DatPhong,
+        id=dat_phong_id,
+        dang_o=True
+    )
